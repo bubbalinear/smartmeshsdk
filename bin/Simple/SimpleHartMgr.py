2d7f18473201b3a767ce3dedf473a248e51a97ea
bubbalinear <blindsay@linear.com>
2014-12-19 12:09:10 -0800

    Update SimpleHartMgr.py
    This guy connects to a HART manager, but it doesn't do anything.  I need help with importing the DEFAULT connection parameters correctly.  It works when you type in a real ip address and type in 4445 but I had to hardcode the default user and password because I couldn't import them correctly

--- bin/Simple/SimpleHartMgr.py
+++ bin/Simple/SimpleHartMgr.py
@@ -10,82 +10,64 @@ if __name__ == "__main__":
 
 #============================ imports =========================================
 
+from SmartMeshSDK.HartMgrConnector import HartMgrConnector
 from SmartMeshSDK.ApiDefinition   import ApiDefinition,         \
-                                         HartMoteDefinition
+                                         HartMgrDefinition
 from SmartMeshSDK.ApiException    import CommandError,          \
                                          ConnectionError
 
 #============================ main ============================================
 
-print 'Simple Application which interacts with the HART mote - (c) Dust Networks'
+print 'Simple Application which interacts with the HART manager - (c) Dust Networks'
+                                         
+print '\n\n================== Step 1. Connecting to the manager =============='
 
-print '\n\n================== Step 1. API exploration ========================'
+connect = raw_input('\nDo you want to connect to a manager XML API? [y/n] ')
 
-print '\n=====\nLoad the API definition of the HART mote'
-apidef = HartMoteDefinition.HartMoteDefinition()
+if connect.strip()!='y':
+    raw_input('\nScript ended. Press Enter to exit.')
+    sys.exit()
+
+print '\n=====\nCreating connector'
+connector = HartMgrConnector.HartMgrConnector()
 print 'done.'
 
-print '\n=====\nList all the defined command IDs:'
-print apidef.getIds(ApiDefinition.ApiDefinition.COMMAND)
+hartHost = raw_input('\nEnter the Manager\'s ip address (Factory default is 192.168.99.100) ')
+#hartHost = raw_input('\nEnter the Manager\'s ip address (leave blank for '+DEFAULT_HOST+') ')
+if not hartHost:
+    hartHost = DEFAULT_HOST
+   
+hartPort = raw_input('\nEnter the Manager\'s port (It is normally 4445) ')
+#hartPort = raw_input('\nEnter the Manager\'s port (leave blank for '+str(DEFAULT_PORT)+') ')
+if hartPort:
+    hartPort = int(hartPort)
+else:
+    hartPort = DEFAULT_PORT
 
-print '\n=====\nList all the defined command names:'
-print apidef.getNames(ApiDefinition.ApiDefinition.COMMAND)
 
-print '\n=====\nGet the command name of command ID 4:'
-print apidef.idToName(ApiDefinition.ApiDefinition.COMMAND,
-                      4)
 
-print '\n=====\nGet the command ID of command name \'getNVParameter\':'
-print apidef.nameToId(ApiDefinition.ApiDefinition.COMMAND,
-                      ['getNVParameter'])
+print '\n=====\nConnecting to Hart manager'
+try:
+    connector.connect({
+                        'host': hartHost,
+                        'port': hartPort,
+                        'user':     'admin',
+                        'password': 'admin',
+                        'use_ssl':  False,
+                     })
+except ConnectionError as err:
+    print err
+    raw_input('\nScript ended. Press Enter to exit.')
+    sys.exit(1)
+print 'done.'
 
-print '\n=====\nList the subcommand of command \'getNVParameter\':'
-print apidef.getNames(ApiDefinition.ApiDefinition.COMMAND,
-                      ['getNVParameter'])
+keepgoing = raw_input('\nDo you want to keep going? [y/n] ')
 
-print '\n=====\nGet a description of the getNVParameter.powerInfo command:'
-print apidef.getDescription(ApiDefinition.ApiDefinition.COMMAND,
-                            ['getNVParameter','powerInfo'])
-                      
-print '\n=====\nList the name of the fields in the getNVParameter.powerInfo request:'
-print apidef.getRequestFieldNames(['getNVParameter','powerInfo'])
+if keepgoing.strip()!='y':
+    raw_input('\nScript ended. Press Enter to exit.')
+    sys.exit()
 
-print '\n=====\nList the name of the fields in the getNVParameter.powerInfo response:'
-print apidef.getResponseFieldNames(ApiDefinition.ApiDefinition.COMMAND,
-                                   ['getNVParameter','powerInfo'])
+###########
 
-print '\n=====\nList the name of the fields in the testRadioTx response:'
-print apidef.getResponseFieldNames(ApiDefinition.ApiDefinition.COMMAND,
-                                   ['testRadioTx'])
-
-print '\n=====\nPrint the format of the testRadioTx \'RC\' response field:'
-print apidef.getResponseFieldFormat(ApiDefinition.ApiDefinition.COMMAND,
-                                    ['testRadioTx'],
-                                    'RC')
-
-print '\n=====\nPrint the valid options of the testRadioTx \'RC\' response field:'
-print apidef.getResponseFieldOptions(ApiDefinition.ApiDefinition.COMMAND,
-                                    ['testRadioTx'],
-                                    'RC').validOptions
-
-print '\n=====\nPrint the format of the getNVParameter.powerInfo \'powerSource\' response field:'
-print apidef.getResponseFieldFormat(ApiDefinition.ApiDefinition.COMMAND,
-                                    ['getNVParameter','powerInfo'],
-                                    'powerSource')
-
-print '\n=====\nPrint the length of the getNVParameter.powerInfo \'powerSource\' response field:'
-print apidef.getResponseFieldLength(ApiDefinition.ApiDefinition.COMMAND,
-                                    ['getNVParameter','powerInfo'],
-                                    'powerSource')
-
-print '\n=====\nPrint the valid options of the getNVParameter.powerInfo \'powerSource\' response field:'
-print apidef.getResponseFieldOptions(ApiDefinition.ApiDefinition.COMMAND,
-                                    ['getNVParameter','powerInfo'],
-                                    'powerSource').validOptions
-
-print '\n=====\nPrint the description of each valid options of the getNVParameter.powerInfo \'powerSource\' response field:'
-print apidef.getResponseFieldOptions(ApiDefinition.ApiDefinition.COMMAND,
-                                    ['getNVParameter','powerInfo'],
-                                    'powerSource').optionDescs
 
 raw_input('\nScript ended. Press Enter to exit.')
